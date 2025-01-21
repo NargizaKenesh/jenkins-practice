1cf70e161e939deb09240c7eb6fcc4abda08f30e
Your Name <your.email@example.com>
2025-01-20 19:10:49 -0600

    smoke tcs to be added

--- tests/day04/web-tables.spec.js
+++ tests/day04/web-tables.spec.js
@@ -1,48 +1,43 @@
 import { expect, test } from '@playwright/test';
 
-test('Web Table Practice', async ({ page }) => {
-    
-    await page.goto("https://practice.cydeo.com/web-tables");
+test("Web Table Practice @smoke", async ({ page }) => {
+  await page.goto("https://practice.cydeo.com/web-tables");
 
-    let table = page.locator("//table[@id='ctl00_MainContent_orderGrid']");
-    
-    let rows = await table.locator("//tr").all();
+  let table = page.locator("//table[@id='ctl00_MainContent_orderGrid']");
 
-    let columns = await table.locator("//th").all();
+  let rows = await table.locator("//tr").all();
 
-    let cells = await table.locator("//td").all();
+  let columns = await table.locator("//th").all();
 
-    expect(rows.length).toBe(9);
-    expect(columns.length).toBe(13);
-    expect(cells.length).toBe(104);
+  let cells = await table.locator("//td").all();
 
-    for(let cell of cells){
-        console.log(await cell.textContent());
-    }
+  expect(rows.length).toBe(9);
+  expect(columns.length).toBe(13);
+  expect(cells.length).toBe(104);
 
+  for (let cell of cells) {
+    console.log(await cell.textContent());
+  }
 });
 
 
-test('Web Table Practice2', async ({ page }) => {
-     
-    await page.goto("https://practice.cydeo.com/web-tables");
+test("Web Table Practice2 @smoke", async ({ page }) => {
+  await page.goto("https://practice.cydeo.com/web-tables");
 
-    let table = page.locator("//table[@id='ctl00_MainContent_orderGrid']");
-    
-    let rows = await table.locator("//tr").all();
+  let table = page.locator("//table[@id='ctl00_MainContent_orderGrid']");
 
-    // create a loop that can print each cell's data of each row.
-    for(let row of rows){
-        let cells = await row.locator("//td").all();
-        if (cells.length > 2) {
-            for(let i = 1; i < cells.length - 1; i++){
-                console.log(await cells[i].textContent());
-            }
-            console.log("-----------------------------");
-        }
+  let rows = await table.locator("//tr").all();
+
+  // create a loop that can print each cell's data of each row.
+  for (let row of rows) {
+    let cells = await row.locator("//td").all();
+    if (cells.length > 2) {
+      for (let i = 1; i < cells.length - 1; i++) {
+        console.log(await cells[i].textContent());
+      }
+      console.log("-----------------------------");
     }
-
-
+  }
 });
 
 
--- tests/day04/window-popup.spec.js
+++ tests/day04/window-popup.spec.js
@@ -1,27 +1,23 @@
 import { expect, test } from '@playwright/test';
 
-test('Window pop-up practice', async ({ page }) => {
-    // creating event listenr for monitoring window pop-ups
-    let promisedNewPageEvent = page.waitForEvent("popup");
+test("Window pop-up practice @smoke", async ({ page }) => {
+  // creating event listenr for monitoring window pop-ups
+  let promisedNewPageEvent = page.waitForEvent("popup");
 
-    await page.goto("https://practice.cydeo.com/windows");
+  await page.goto("https://practice.cydeo.com/windows");
 
-    await page.click("text='Click Here'"); // triggers the pop-up event
+  await page.click("text='Click Here'"); // triggers the pop-up event
 
-    let newPage = await promisedNewPageEvent; // await for the prose to be resolved
+  let newPage = await promisedNewPageEvent; // await for the prose to be resolved
 
-    await expect(newPage).toHaveTitle("New Window");
-    await expect(page).toHaveTitle("Windows");
-    
-    await page.bringToFront();
-    let firstWindowElement = page.getByText("Opening a new window");
-    await expect(firstWindowElement).toBeVisible();
+  await expect(newPage).toHaveTitle("New Window");
+  await expect(page).toHaveTitle("Windows");
 
+  await page.bringToFront();
+  let firstWindowElement = page.getByText("Opening a new window");
+  await expect(firstWindowElement).toBeVisible();
 
-    await newPage.bringToFront();
-    let secondWindowElement = newPage.getByText("New Window");
-    await expect(secondWindowElement).toBeVisible();
-  
-
-  
+  await newPage.bringToFront();
+  let secondWindowElement = newPage.getByText("New Window");
+  await expect(secondWindowElement).toBeVisible();
 });
